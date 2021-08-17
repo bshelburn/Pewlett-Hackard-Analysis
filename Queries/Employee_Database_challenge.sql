27c8bef6e4de74a1f850f60cceaf95f6cbd05467
bshelburn <84201082+bshelburn@users.noreply.github.com>
2021-08-17 11:52:49 -0400

    Add files via upload

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,73 @@
+drop table retirement_titles
+drop table unique_titles
+DROP TABLE retiring_emp_by_title
+drop table retiring_titles
+
+-- deliverable 1 number of retiring employees by title
+
+SELECT em.emp_no, 
+        em.first_name, 
+        em.last_name,
+	    ti.title, 
+        ti.from_date, 
+        ti.to_date
+	INTO retirement_titles
+	FROM employees AS em
+	LEFT JOIN titles as ti
+	ON (em.emp_no = ti.emp_no)
+	WHERE (em.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	ORDER BY em.emp_no;
+
+SELECT DISTINCT ON (emp_no) emp_no, 
+	first_name, 
+    last_name, 
+    title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT COUNT(title), title 
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT em.emp_no, 
+        em.first_name, 
+        em.last_name,
+	    ti.title, 
+        ti.from_date, 
+        ti.to_date
+	INTO retiring_emp_by_title
+	FROM employees AS em
+	LEFT JOIN titles as ti
+	ON (em.emp_no = ti.emp_no)
+	WHERE (em.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	ORDER BY em.emp_no;
+
+SELECT DISTINCT ON (emp_no) emp_no, 
+	first_name, 
+    last_name, 
+    title
+INTO unique_titles
+FROM retiring_emp_by_title
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT COUNT(title), title 
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+--part 2 Employees Eligible for the Mentorship Program
+
+SELECT DISTINCT ON (em.emp_no) em.emp_no, em.first_name, em.last_name, em.birth_date, de.from_date, de.to_date, ti.title
+INTO mentorship_elegible 
+FROM employees AS em
+LEFT JOIN dept_emp AS de
+ON em.emp_no = de.emp_no
+LEFT JOIN titles AS ti
+ON em.emp_no = ti.emp_no
+WHERE (em.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY em.emp_no;
